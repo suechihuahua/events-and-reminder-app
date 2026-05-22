1acc7b0b3b4390c0d31dd68e1d95b5b68ce6ada2
suechihuahua <149025705+suechihuahua@users.noreply.github.com>
2026-05-22 17:06:07 +0800

    Changed Title of the app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>My APP</Text>
+      <Text>Events and Reminder App</Text>
     </View>
   );
 }
